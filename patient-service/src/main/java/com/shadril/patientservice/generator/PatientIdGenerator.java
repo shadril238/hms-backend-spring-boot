5e5644c234223c5a3923993936bd61a8b1ec3d15
Shadril Hassan Shifat <shadril.shifat@bjitacademy.com>
2023-11-10 11:34:44 +0600

    Fixed the bugs for patient id generator in patient-service

--- patient-service/src/main/java/com/shadril/patientservice/generator/PatientIdGenerator.java
+++ patient-service/src/main/java/com/shadril/patientservice/generator/PatientIdGenerator.java
@@ -1,19 +1,43 @@
 package com.shadril.patientservice.generator;
 
+import com.shadril.patientservice.repository.PatientRepository;
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
-
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 import java.io.Serializable;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class PatientIdGenerator implements IdentifierGenerator {
+@Component
+public class PatientIdGenerator implements IdentifierGenerator, ApplicationContextAware {
 
-    private final AtomicInteger counter = new AtomicInteger(0);
+    private static ApplicationContext context;
+
+    private AtomicInteger counter = null;
+
     @Override
     public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
-        // Generate a unique 5-digit ID for the patient
+        if (counter == null) {
+            synchronized (this) {
+                if (counter == null) {
+                    PatientRepository patientRepository = context.getBean(PatientRepository.class);
+                    String maxPatientId = patientRepository.findMaxPatientId()
+                            .orElse("00-00000");
+                    int maxId = Integer.parseInt(maxPatientId.substring(3));
+                    counter = new AtomicInteger(maxId);
+                }
+            }
+        }
+
         String uniqueId = String.format("%05d", counter.incrementAndGet());
         return "00-" + uniqueId;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        PatientIdGenerator.context = applicationContext;
     }
 }
--- patient-service/src/main/java/com/shadril/patientservice/repository/PatientRepository.java
+++ patient-service/src/main/java/com/shadril/patientservice/repository/PatientRepository.java
@@ -2,7 +2,11 @@ package com.shadril.patientservice.repository;
 
 import com.shadril.patientservice.entities.PatientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface PatientRepository extends JpaRepository<PatientEntity, String> {
-
+    @Query("SELECT MAX(p.patientId) FROM PatientEntity p")
+    Optional<String> findMaxPatientId();
 }
