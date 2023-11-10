72ad8aad4486adb708fe70dd2fe141083433fa0c
Shadril Hassan Shifat <shadril.shifat@bjitacademy.com>
2023-11-10 17:23:44 +0600

    Configured authorization filter in patient-service

--- patient-service/src/main/java/com/shadril/patientservice/security/WebSecurityConfig.java
+++ patient-service/src/main/java/com/shadril/patientservice/security/WebSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -33,7 +34,7 @@ public class WebSecurityConfig {
                     auth
                             .anyRequest().permitAll();
                 })
-//                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
         ;
         return http.build();
     }
