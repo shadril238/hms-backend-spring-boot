894c7e03184e544bc1972dbef8bb3565d560905a
Shadril Hassan Shifat <shadril.shifat@bjitacademy.com>
2023-11-15 14:55:50 +0600

    init cdss-service

---
+++ cdss-service/src/main/java/com/shadril/cdssservice/CdssServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shadril.cdssservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdssServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdssServiceApplication.class, args);
+	}
+
+}
---
+++ cdss-service/src/test/java/com/shadril/cdssservice/CdssServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadril.cdssservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdssServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
