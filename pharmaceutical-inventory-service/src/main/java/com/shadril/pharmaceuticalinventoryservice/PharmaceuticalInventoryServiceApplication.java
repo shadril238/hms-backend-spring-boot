75f90f6016daf4d5063f77c71135383fb873c6aa
Shadril Hassan Shifat <shadril.shifat@bjitacademy.com>
2023-11-15 14:59:04 +0600

    init pharmaceutical-inventory-service

---
+++ pharmaceutical-inventory-service/src/main/java/com/shadril/pharmaceuticalinventoryservice/PharmaceuticalInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shadril.pharmaceuticalinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaceuticalInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaceuticalInventoryServiceApplication.class, args);
+	}
+
+}
