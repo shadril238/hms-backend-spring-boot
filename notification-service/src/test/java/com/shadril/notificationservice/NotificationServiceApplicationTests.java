ac49466493619af4541296a5bad135a76a99ad05
Shadril Hassan Shifat <shadril.shifat@bjitacademy.com>
2023-11-15 15:06:14 +0600

    init notification-service

---
+++ notification-service/src/test/java/com/shadril/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadril.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
