ef7175344b98eea0ba9eaf0a21e0de53a18538f5
Shadril Hassan Shifat <shadrilhassan@outlook.com>
2023-11-20 12:20:40 +0600

    controller

---
+++ pharmaceutical-inventory-service/src/main/java/com/shadril/pharmaceuticalinventoryservice/controller/PharmaceuticalInventoryController.java
@@ -0,0 +1,9 @@
+package com.shadril.pharmaceuticalinventoryservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class PharmaceuticalInventoryController {
+}
