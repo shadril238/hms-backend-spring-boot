f99abd00be1e492d1d2e420ca0ae99a4ddb39882
Shadril Hassan Shifat <shadrilhassan@outlook.com>
2023-11-26 20:21:11 +0600

    modified so that latest post will be shown in community portal service

--- community-portal-service/src/main/java/com/shadril/communityportalservice/service/implementation/PostServiceImplementation.java
+++ community-portal-service/src/main/java/com/shadril/communityportalservice/service/implementation/PostServiceImplementation.java
@@ -16,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -100,6 +101,7 @@ public class PostServiceImplementation implements PostService {
 
             return postEntities.stream()
                     .filter(PostEntity::isActive)
+                    .sorted(Comparator.comparing(PostEntity::getCreatedAt).reversed())
                     .map(postEntity -> modelMapper.map(postEntity, PostDto.class))
                     .collect(Collectors.toList());
         } catch (CustomException ex) {
