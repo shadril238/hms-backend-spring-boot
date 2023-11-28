bcae39124ac1006fc0f2db9ced7a3be6eea1d264
Shadril Hassan Shifat <shadril.shifat@bjitacademy.com>
2023-11-28 23:39:56 +0600

    unit testing

--- patient-service/src/test/java/com/shadril/patientservice/PatientServiceImplementationTest.java
+++ patient-service/src/test/java/com/shadril/patientservice/PatientServiceImplementationTest.java
@@ -1,9 +1,5 @@
 package com.shadril.patientservice;
 
-import com.shadril.patientservice.dto.PatientRegistrationRequestDto;
-import com.shadril.patientservice.dto.UserRegistrationResponseDto;
-import com.shadril.patientservice.enums.BloodGroup;
-import com.shadril.patientservice.enums.Gender;
 import com.shadril.patientservice.exception.CustomException;
 import com.shadril.patientservice.networkmanager.SecurityServiceFeignClient;
 import com.shadril.patientservice.repository.PatientRepository;
@@ -25,9 +21,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PatientServiceImplementationTest {
@@ -92,5 +87,92 @@ public class PatientServiceImplementationTest {
 
         patientService.approvePatient("123");
         assertEquals(true, patientEntity.isApproved());
+    }
+
+    @Test
+    public void testSearchPatientByName() throws CustomException {
+        String searchName = "TestName";
+        PatientEntity patient1 = new PatientEntity();
+        PatientEntity patient2 = new PatientEntity();
+        List<PatientEntity> patientEntities = Arrays.asList(patient1, patient2);
+
+        PatientDto patientDto1 = new PatientDto();
+        PatientDto patientDto2 = new PatientDto();
+
+        when(patientRepository.searchByFirstNameOrLastNameContainingIgnoreCase(searchName))
+                .thenReturn(patientEntities);
+        when(modelMapper.map(patient1, PatientDto.class)).thenReturn(patientDto1);
+        when(modelMapper.map(patient2, PatientDto.class)).thenReturn(patientDto2);
+
+        List<PatientDto> result = patientService.searchPatientByName(searchName);
+
+        assertEquals(2, result.size());
+        assertTrue(result.contains(patientDto1));
+        assertTrue(result.contains(patientDto2));
+        verify(patientRepository).searchByFirstNameOrLastNameContainingIgnoreCase(searchName);
+        verify(modelMapper, times(2)).map(any(PatientEntity.class), eq(PatientDto.class));
+    }
+
+    @Test
+    public void testGetAllApprovedPatients() throws CustomException {
+        PatientEntity approvedPatient1 = new PatientEntity();
+        PatientEntity approvedPatient2 = new PatientEntity();
+        List<PatientEntity> approvedPatients = Arrays.asList(approvedPatient1, approvedPatient2);
+
+        PatientDto patientDto1 = new PatientDto();
+        PatientDto patientDto2 = new PatientDto();
+
+        when(patientRepository.findAllByApproved(true)).thenReturn(approvedPatients);
+        when(modelMapper.map(approvedPatient1, PatientDto.class)).thenReturn(patientDto1);
+        when(modelMapper.map(approvedPatient2, PatientDto.class)).thenReturn(patientDto2);
+
+        List<PatientDto> result = patientService.getAllApprovedPatients();
+
+        assertEquals(2, result.size());
+        assertTrue(result.containsAll(Arrays.asList(patientDto1, patientDto2)));
+        verify(patientRepository).findAllByApproved(true);
+        verify(modelMapper, times(2)).map(any(PatientEntity.class), eq(PatientDto.class));
+    }
+
+    @Test
+    public void testGetAllUnapprovedPatients() throws CustomException {
+        PatientEntity unapprovedPatient1 = new PatientEntity();
+        PatientEntity unapprovedPatient2 = new PatientEntity();
+        List<PatientEntity> unapprovedPatients = Arrays.asList(unapprovedPatient1, unapprovedPatient2);
+
+        PatientDto patientDto1 = new PatientDto();
+        PatientDto patientDto2 = new PatientDto();
+
+        when(patientRepository.findAllByApproved(false)).thenReturn(unapprovedPatients);
+        when(modelMapper.map(unapprovedPatient1, PatientDto.class)).thenReturn(patientDto1);
+        when(modelMapper.map(unapprovedPatient2, PatientDto.class)).thenReturn(patientDto2);
+
+
+        List<PatientDto> result = patientService.getAllUnapprovedPatients();
+
+        assertEquals(2, result.size());
+        assertTrue(result.containsAll(Arrays.asList(patientDto1, patientDto2)));
+        verify(patientRepository).findAllByApproved(false);
+        verify(modelMapper, times(2)).map(any(PatientEntity.class), eq(PatientDto.class));
+    }
+
+    @Test
+    public void testCountTotalPatients() throws CustomException {
+        long expectedCount = 10L;
+        when(patientRepository.count()).thenReturn(expectedCount);
+
+        long actualCount = patientService.countTotalPatients();
+
+        assertEquals(expectedCount, actualCount);
+        verify(patientRepository).count();
+    }
+
+    @Test
+    public void testGetPatientByIdNotFound() {
+        String patientId = "notFoundId";
+        when(patientRepository.findById(patientId)).thenReturn(Optional.empty());
+
+        assertThrows(CustomException.class, () -> patientService.getPatientById(patientId));
+        verify(patientRepository).findById(patientId);
     }
 }
